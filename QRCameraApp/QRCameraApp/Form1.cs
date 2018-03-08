042bc4224012e5956f0d1854e25d555b8d44cfd7
botbot seki <sekibotbot@gmail.com>
2018-03-08 16:22:30 +0900

    qr読み取り完成

--- QRCameraApp/QRCameraApp/Form1.cs
+++ QRCameraApp/QRCameraApp/Form1.cs
@@ -3,44 +3,78 @@ using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Text;
 using OpenCvSharp;
+using ZXing;
+using System.Drawing;
 
 namespace QRCameraApp
 {
     public partial class Form1 : Form
     {
+        private BarcodeReader barcodeReader = new BarcodeReader();
+        private Bitmap img = new Bitmap(@"C:\Users\sekib\Pictures\Saved Pictures\qr.bmp");
         public Form1()
         {
             InitializeComponent();
+
+            barcodeReader.AutoRotate = true;
+            barcodeReader.TryInverted = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.Text = "QRCamerra";
         }
 
-
-
-        private void Cam(object sender, EventArgs e)
+        private string readQR(Bitmap bmp)
         {
+            string ret = "(not found)";
 
-            Mat src = new Mat(@"C:\Users\sekib\Pictures\Saved Pictures\1.png", ImreadModes.GrayScale);   // OpenCvSharp 3.x
-            //Mat src = new Mat("lenna.png", LoadMode.GrayScale); // OpenCvSharp 2.4.x
-            Mat dst = new Mat();
-
-            Cv2.Canny(src, dst, 50, 200);
-            
-            //using (new Window("src image", src))
-            //using (new Window("dst image", dst))
+            try
             {
-                Cv2.WaitKey();
+                Result result = barcodeReader.Decode(bmp);
+                if (result != null)
+                {
+                    ret = result.Text;
+                }
+            }
+            catch
+            {
+                //ZXingのバグ？barcodeReader.Decodeに失敗することがある ver 0.14.0.0 で確認
             }
 
+            return ret;
         }
+    
+
+    //private void Cam(object sender, EventArgs e)
+    //    {
+
+    //        Mat src = new Mat(@"C:\Users\sekib\Pictures\Saved Pictures\1.png", ImreadModes.GrayScale);   // OpenCvSharp 3.x
+    //        //Mat src = new Mat("lenna.png", LoadMode.GrayScale); // OpenCvSharp 2.4.x
+    //        Mat dst = new Mat();
+
+    //        Cv2.Canny(src, dst, 50, 200);
+            
+    //        //using (new Window("src image", src))
+    //        //using (new Window("dst image", dst))
+    //        {
+    //            Cv2.WaitKey();
+    //        }
+
+    //    }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Cam(sender,e);
+            Console.WriteLine("click");
+            string hoge = readQR(img);
+            Console.WriteLine(hoge);
+           // Cam(sender,e);
         }
+
+
+
+
+
 
     }
 }
